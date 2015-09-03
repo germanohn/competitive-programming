3e5c6335599939b94818198efd4df94bad5d4a39
Germano <germanohn@hotmail.com>
2015-09-03 17:09:08 -0300

    tesouro

--- tobi/tesouro.cpp
+++ tobi/tesouro.cpp
@@ -1,37 +1,35 @@
-#include <cstdio>
-#include <cstring>
-#include <algorithm>
-#define MAXN 10500
-#define MAX 110 
-
-using namespace std;
+#include <stdio.h>
+#include <string.h>
+#define MAXN 10000
+#define MAX 105 
 
 int x, y, n, v[MAX], memo[MAX][MAXN], tot;
 
 int dp (int i, int soma) {
+    if (soma == tot) return 1;
+    if (i >= n || soma > tot) return 0;
     if (memo[i][soma] != -1) return memo[i][soma];
-    if (i > n || soma > tot/2) return 0;
-    if (soma == tot/2) return 1;
     
     return memo[i][soma] = dp (i + 1, soma + v[i]) + dp (i + 1, soma); 
 } 
 
 
 int main () {
-    int cont = 1;
+    int cont = 1, i;
     while (scanf ("%d %d %d", &x, &y, &n) && n != 0 && x != 0 && y != 0) {
+        memset (memo, -1, sizeof (memo));
         tot = 0;
         printf ("Teste %d\n", cont);
-        memset (memo, -1, sizeof (memo));
-        for (int i = 0; i < n; i++) {
+        for (i = 0; i < n; i++) {
             scanf ("%d", &v[i]);
             tot += v[i];
         }  
         tot += x + y;
         if (tot % 2 != 0) {
             printf ("N\n\n");
-        }    
+        }
         else {        
+            tot = tot/2;    
             if (dp (0, x)) printf ("S\n\n");
             else printf ("N\n\n");           
         }        
