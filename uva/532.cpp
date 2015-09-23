145dd2f419c2a127326b58d9aa0f4c9cd850b9ba
Germano <germanohn@hotmail.com>
2015-09-23 14:40:43 -0300

    532.cpp

--- uva/532.cpp
+++ uva/532.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-int l, r, c;
+int l, r, c, cont, m[32][32][32];
 int iniL, iniR, iniC;
 int fimL, fimR, fimC;
-int ll[3] = {-1, 0, 1}
-int rr[4] = {-1, 0, 1, 0}
-int cc[4] = {0, 1, 0, -1}
-char m[32][32][32];
+int mx[6] = {0, 0, 0, 0, -1, 1};
+int my[6] = {0, 0, -1, 1, 0, 0};
+int mz[6] = {-1, 1, 0, 0, 0, 0};
+char d;
 
 struct no{
     int i, j, k;
@@ -18,53 +18,66 @@ no pos;
 
 int bfs () {
     queue<no> q;
-    pos.i = iniL;
-    pos.j = iniR;
-    pos.k = iniC;
+    pos.i = iniR;
+    pos.j = iniC;
+    pos.k = iniL;
     q.push (pos);
     while (!q.empty ()) {
-        int x = q.front ().i
-        int y = q.front ().j
-        int z = q.front ().z
+        int x = q.front ().i;
+        int y = q.front ().j;
+        int z = q.front ().k;
         q.pop ();
-        if (m[x][y][z] == 'e'
-        for (int a = 0; a < 3; a++) {
-            for (int b = 0; b < 4; b++) {
-                if (x + ll[a] >= 0 && x + ll[a] <= 2 &&
-                    y + rr[b] >= 0 && y + rr[b] <= 3 &&
-                    z + cc[b] >= 0 && z + cc[b] <= 3) {
-                    if (m[x + ll[a]][y + rr[b]][z + cc[b]] != '#') 
-                        m[x + ll[a]][y + rr[b]][z + cc[b]] = m[x][y][z] + 1;
-                }    
-                pos.i = x + ll[a];
-                pos.j = y + rr[b];
-                pos.k = z + cc[b];
-                q.push (pos);
-            }
+        if (x == fimR && y == fimC && z == fimL) {
+            cont = m[x][y][z];
+            return 1;
         }
+        for (int a = 0; a < 6; a++) {
+                if (x + mx[a] >= 0 && x + mx[a] < r &&
+                    y + my[a] >= 0 && y + my[a] < c &&
+                    z + mz[a] >= 0 && z + mz[a] < l) {
+                    int &kk = m[x + mx[a]][y + my[a]][z + mz[a]];
+                    if (m[x][y][z] + 1 < kk) { 
+                        kk = m[x][y][z] + 1;
+                        pos.i = x + mx[a];
+                        pos.j = y + my[a];
+                        pos.k = z + mz[a];
+                        q.push (pos);
+                    }
+                }    
+        }
+        
     }
+    return 0;
 }   
 
 int main () {
     while (scanf ("%d %d %d", &l, &r, &c) && l != 0) {
-        for (int i = 0; i < l; i++) 
-            for (int j = 0; j < r; j++) 
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < r; j++) {
                 for (int k = 0; k < c; k++) {
-                    scanf ("%c", &m[i][j][k]);
-                    if (m[i][j][k] == S) {
+                    scanf (" %c", &d);
+                    if (d == 'S') {
                         iniL = i;
                         iniR = j;
                         iniC = k;
-                        m[i][j][k] = 0;
+                        m[j][k][i] = 0;
                     }
-                    else if (m[i][j][k] == E) {
+                    else if (d == 'E') {
                         fimL = i; 
                         fimR = j;
                         fimC = k;
+                        m[j][k][i] = INT_MAX;
+                        
                     }
+                    else if (d == '.')
+                        m[j][k][i] = INT_MAX;
+                    else if (c == '#')
+                        m[j][k][i] = -1;                    
                 }
+            }
+        }
         if (bfs ())
-            printf ("Escaped in %d minute(s).", cont);
+            printf ("Escaped in %d minute(s).\n", cont);
         else
             printf ("Trapped!\n");
     }
