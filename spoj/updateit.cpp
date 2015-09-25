677f072d0e990472aa552d7d22a71143c164bb2a
Germano <germano@donkeykong.linux.ime.usp.br>
2015-09-25 13:51:15 -0300

    comp

--- spoj/updateit.cpp
+++ spoj/updateit.cpp
@@ -3,31 +3,37 @@
 
 using namespace std;
 
-int v[MAX], bit[MAX];
+int bit[MAX];
 int t, n, u, l, r, val, q, idx;
 
-void query (int ind) {
+void update (int idx, int val) {
+    for (int i = idx; i <= n; i += i & -i) 
+        bit[i] += val;
+}
+
+int query (int idx) {
     int sum = 0; 
-    for (int i = ind; i > 0; i -= i & -i)
+    for (int i = idx; i > 0; i -= i & -i)
         sum += bit[i];
     return sum;
 }
-
-int update (int ini, int fim, int val
 
 int main () {
     scanf ("%d", &t);
     while (t--) {
         scanf ("%d %d", &n, &u);
-        for (int i = 1; i <= n; i++)
-            v[i] = 0;
+        for (int i = 0; i <= n; i++)
+            bit[i] = 0;
         for (int j = 1; j <= u; j++) {
             scanf ("%d %d %d", &l, &r, &val);
-            update (l, r, val);
+            update (l + 1, val);
+            update (r + 1, -val);
         }
+        scanf ("%d", &q);
         for (int i = 0; i < q; i++) {
             scanf ("%d", &idx);
-            query (idx);
+            printf ("idx %d\n", idx);
+            printf ("%d\n", query (idx + 1) - query(idx));
         }   
     }
 
