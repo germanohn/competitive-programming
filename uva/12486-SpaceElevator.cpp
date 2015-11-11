5093f795813a8fb43eaed5e6a6f77c691d027aab
Germano <germanohn@hotmail.com>
2015-11-11 14:01:24 -0200

    elevador

--- uva/12486-SpaceElevator.cpp
+++ uva/12486-SpaceElevator.cpp
@@ -8,30 +8,23 @@ llu me[20][10][2][2];
 char str[20];
 
 llu dp (int i, int ult, bool pre, bool f) {
-    if (str[i] == 0) {
-        if (f) {
-            printf ("cont %d\n", cont++);
-            return 1;
-        }
-        printf ("ult %d\n", ult);
-        return 0;
-    }
+    if (str[i] == 0) return f; 
     
     //if (str[0] == 0 && !f) return 0;
     llu &m = me[i][ult][pre][f];
     if (m != -1) return m;
     m = 0;
     for (int d = 0; d <= 9; d++) {
-        f = true;
+        f = false;
         if (pre && d > str[i]-'0') break;
-        if (d == 4 || (ult == 1 && d == 3) || (i == 0 && d == 0)) {
-            printf ("passei pelo falso\n");
-            f = false;
+        if (d == 4 || (ult == 1 && d == 3)) {
+           // printf ("passei pelo falso\n");
+            f = true;
         }
         m += dp (i+1, d, pre && d == str[i]-'0', f);
-        printf ("m %llu\n", m);
+        //printf ("m %llu\n", m);
     }
-    printf ("i %d m %llu\n", i, m);
+    //printf ("i %d m %llu\n", i, m);
     return m;
 } 
 
@@ -39,9 +32,18 @@ int main () {
     while (scanf ("%llu", &n) != EOF) {
         cont = 0;
         sprintf (str, "%llu", n);
-        printf ("str %s\n", str);
+        //printf ("str %s\n", str);
         memset (me, -1, sizeof (me));
-        ans = dp (0, 0, true, true);
-        printf ("%llu\n", ans);
+        llu e = 0, d = 1000000000000000000;
+        while (e != d) {
+            llu mid = e + (d-e)/2;
+            sprintf (str, "%llu", mid);
+            memset (me, -1, sizeof (me));
+            llu quant = dp (0, 0, true, false);
+            //printf ("quant %llu ", quant);
+            if (mid - quant < n) d = mid-1;
+            else e = mid;
+        }
+        printf ("%llu\n", e);
     }
 }
