b53bd92ef69fe1360d7586e34c48aec1b40d6388
Germano <germanohn@hotmail.com>
2015-11-27 17:19:57 -0200

    penguin

--- codeforces/a.edu2.cpp
+++ codeforces/a.edu2.cpp
@@ -10,48 +10,63 @@ char a[MAX], b[MAX], s[MAX], aux[MAX];
 
 int main () {
     scanf (" %s", s);
-    bool fa = true, fb = true, ff;
+    bool fa = true, fb = true, ff, pri = true;;
     bool A = false, B = false, esp, ant;
     int af = 0, bf = 0, i = 0;
     int cont = 1;
     while (s[i] != '\0') {
         int k = 0; 
         esp = true;
-        A = false, B = false, ff = false, ant = true; 
+        if (!pri) i++;
+        pri = false;
+        A = false, B = false, ff = false, ant = false;
+        int quant = 0; 
         while (s[i] != '\0') {
-            printf ("s[i] %c\n", s[i]);
+            //printf ("s[i] %c\n", s[i]);
+            //if (quant >= 1) {
+                //ant = false;
+              //  printf ("ant vira false\n");
+            //}
+            if (s[i] == '.') aux[k++] = s[i];
             if (s[i] == ',' || s[i] == ';' || s[i] == '.') {
-                printf ("sair\n");
+                //printf ("sair\n");
                 break;
             }
             ant = false;
             aux[k++] = s[i];
             if (!ff && s[i] == '0') {
                 ff = true;
-                printf ("pri e zero\n");
-                esp = false;
+                //printf ("pri e zero\n");
+             
             }
             else if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')) {
                 B = true;
-                printf ("letra\n");
+                //printf ("letra\n");
                 esp = false;
             }
             else if (ff && s[i] != '0') {
                 B = true;
-                printf ("leading\n");
+                //printf ("leading\n");
                 esp = false;
             }
             i++;
+            quant++;
         }
-        if (ant) esp = false;
-        if (!B && esp) A = true;
-        printf ("cont %d aux %s\n", cont, aux);
+
+        if (ant) {
+            esp = false;
+            //printf ("\npassou\n");
+        }
+        if (!B && esp && quant != 0) A = true;
+        else if (quant != 0) B = true;
+        //printf ("cont %d aux %s\n", cont, aux);
         /*if (cont == 3) {
         printf ("pri\n");
         return 0;
         }*/
         if (A && esp) {
             if (esp) printf ("espaco true\n");
+            printf ("entrou no A\n");
             if (fa == true) {
                 a[0] = '"';
                 af++;
@@ -66,7 +81,8 @@ int main () {
             } 
             af = af+k;
         }
-        else if (B || !esp) {
+        else if (B || !esp || s[i] == '.') {
+            printf ("entrou no B\n");
             if (!esp) printf ("espaco false\n");
             if (fb == true) {
                 b[0] = '"';
@@ -82,7 +98,7 @@ int main () {
             }
             bf = bf+k;
         }
-        i++;
+        
         cont++;
     }
     if (strlen (a) > 1) a[af] = '"';
--- timus/1709-Penguin.cpp
+++ timus/1709-Penguin.cpp
@@ -5,13 +5,14 @@
 #define mp make_pair
 using namespace std;
 typedef pair<int, int> pii;
+typedef long long ll;
 
 const int MAX = 105;
 
-int n, d, a;
+ll n, d, a;
 int pai[MAX], peso[MAX];
 char rta[MAX][MAX], A[MAX][MAX];
-vector<pii> adj;
+
 
 int find (int x) {
     if (pai[x] == -1) return x;
@@ -21,48 +22,50 @@ int find (int x) {
 bool join (int x, int y) {
     x = find (x), y = find (y);
     if (x == y) return false;
-    if (x < y) swap (x, y);
+    if (peso[x] < peso[y]) swap (x, y);
     pai[y] = x;
     peso[x] += peso[y];
     return true;
 }
 
 int main () {
-    scanf ("%d%d%d", &n, &d, &a);//d: price to cancel the flight
+    scanf ("%lld%lld%lld", &n, &d, &a);//d: price to cancel the flight
     //a price to create a new flight
     int fly;
     for (int i = 0; i <= n; i++) {
         pai[i] = -1;
         peso[i] = 1;
     }
-    int k = 1;
-    for (int i = 0; i < n; i++) {
-        for (int j = k; j < n; j++) {
-            scanf (" %c", &A[i][j]);
+    for (int i = 0; i < n; i++) 
+        for (int j = 0; j < n; j++)
             rta[i][j] = '0';
+    long long cost = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf (" %c", &A[i][j]);
+            if (i < j) continue;
             if (A[i][j] == '1') {
-                printf ("colocou i %d j %d\n", i, j);
-                adj.pb (mp (i, j));
-                adj.pb (mp (j, i));
-                if (!join (x, y)) {
+                //printf ("colocou i %d j %d\n", i, j);
+                if (!join (i, j)) {
                     cost += d;
-                    rta[x][y] = 'd';
-                    rta[y][x] = 'd';
+                    rta[i][j] = 'd';
+                    rta[j][i] = 'd';
                 }
             }
         }
-        k++;
     }
-    printf ("cost de tirar %d\n", cost);
+    //printf ("cost de tirar %d\n", cost);
+    int p = find (0), quant = 0;
     for (int i = 1; i < n; i++) {
-        if (pai[i] == -1) {
-            join (pai[0], pai[i]);
-            cost += a;
-
+        if (pai[i] == -1 && i != p) {
+            quant++;
+            rta[i][p] = 'a';
+            rta[p][i] = 'a';
         }
     }
-    printf ("%d\n", cost);
-    for (int i = 0; i< n; i++) {
+    cost += (quant) * a;
+    printf ("%lld\n", cost);
+    for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             printf ("%c", rta[i][j]);
         }
