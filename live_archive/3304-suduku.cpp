30a3f52c96756f921595e5e582653fc70e1abc57
Germano <germanohn@hotmail.com>
2015-11-28 12:21:40 -0200

    foi o suduku po

--- live_archive/3304-suduku.cpp
+++ live_archive/3304-suduku.cpp
@@ -1,53 +1,72 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int t;
-int A[10][10];
+const int MAX = 10;
 
-bool go (int row, int col, int val) {
-    for (int j = 1; j <= 9; j++) {
-        if (A[row][j] == val) return false;
-    }
-    for (int i = 1; i <= 9; i++) {
-        if (A[i][col] == val) return false;
-    }
-    for (int i = row; i <= row+3; i++) {
-        for (int j = col; j <= col+3; j++) {
-            if (A[i][j] == val) return false;
+char A[MAX][MAX];
+int t, grid[MAX][MAX];
+
+bool findEmpty (int &row, int &col) {
+    for (row = 0; row < 9; row++) {
+        for (col = 0; col < 9; col++) {
+            if (grid[row][col] == 0)
+                return true;
         }
     }
+    return false;
 }
 
-void solve () {
-    int k = 1; 
-    for (int i = 1; i <= 9; i++) {
-        for (int j = 1; j <= 9; j++) {
-            if (A[i][j] == 0) {
-                if (go (i, j, k)) {
-                    A[i][j] = k;
-                        
-                }
-            }
+bool isSafe (int num, int &row, int &col) {
+    //row
+    for (int j = 0; j < 9; j++) {
+        if (grid[row][j] == num) 
+            return false;
+    }
+    //col
+    for (int i = 0; i < 9; i++) {
+        if (grid[i][col] == num)
+            return false;
+    }
+    //box
+    int comI = (row/3)*3;
+    int comJ = (col/3)*3;
+    for (int i = comI; i < comI+3; i++) {
+        for (int j = comJ; j < comJ+3; j++) {
+            if (grid[i][j] == num)
+                return false;
         }
     }
+    return true;
 }
 
-int prox (int x, int y) {
-    
+bool solveSuduku () {
+    int row, col;
+    if (!findEmpty (row, col)) 
+        return true;
+    for (int num = 1; num <= 9; num++) {
+        if (isSafe (num, row, col)) {
+            grid[row][col] = num;
+            if (solveSuduku ())
+                return true;
+            grid[row][col] = 0;
+        }
+    }
+    return false;
 }
 
 int main () {
     scanf ("%d", &t);
-    while(t--) {
-        for (int i = 1; i <= 9; i++) {
-            for (int j = 1; j <= 9; j++) {
-                scanf ("%d", &A[i][j]);
+    while (t--) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                scanf (" %c", &A[i][j]);
+                grid[i][j] = A[i][j] - '0';
             }
         }
-        solve ();
-        for (int i = 1; i <= 9; i++) {
-            for (int j = 1; j <= 9; j++) {
-                printf ("%d", A[i][j]);
+        solveSuduku ();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                printf ("%d", grid[i][j]);
             }
             printf ("\n");
         }
