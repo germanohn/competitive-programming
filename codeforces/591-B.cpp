c6c77bb4ca4b7f87c20da77d4e2c42da7f2468ef
Germano <germanohn@hotmail.com>
2016-04-14 23:35:48 -0300

    alternando nomes

---
+++ codeforces/591-B.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define debug (args...) fprintf (stderr, args)
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main () {
+
+}
