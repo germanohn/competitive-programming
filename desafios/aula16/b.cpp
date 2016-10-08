03997b4bd7d1d32b7246ce5abb08097ab62cbeb9
Germano <germanohn8@gmail.com>
2016-10-07 22:29:59 -0300

    terminei aula18 aula16 e aula15

--- desafios/aula16/b.cpp
+++ desafios/aula16/b.cpp
@@ -11,6 +11,15 @@ typedef pair<int, int> pii;
 
 const int MAX = 55;
 
+struct edge {
+    int to, c, f;
+};
+
+int cur, tempo, n, m;
+int seen[MAX], seen_res[MAX];
+edge edges[2*MAX*MAX];
+vector<int> adj[MAX];
+
 int dfs (int u, int mini) {
     seen[u] = tempo;
     if (u == 2) return mini;
@@ -37,16 +46,16 @@ int ford_fulkerson () {
 }
 
 void add_edge (int u, int v, int c, int rc) {
-    edges[cur].to = u, edges[cur].c = c, edges[cur].f = 0;
+    edges[cur].to = v, edges[cur].c = c, edges[cur].f = 0;
     adj[u].pb (cur++);
-    edges[cur].to = v, edges[cur].c = rc, edges[cur].f = 0;
+    edges[cur].to = u, edges[cur].c = rc, edges[cur].f = 0;
     adj[v].pb (cur++);
 } 
 
 void dfs_res (int u) {
     seen_res[u] = true;
     for (int i = 0; i < adj[u].size (); i++) {
-        edge ed = adj[u][i];
+        edge ed = edges[adj[u][i]];
         if (ed.c == ed.f || seen_res[ed.to]) continue;
         dfs_res (ed.to);
     }
@@ -56,15 +65,29 @@ int main () {
     while (scanf (" %d %d", &n, &m)) {
         if (n == 0 && m == 0) break;
 
+        cur = 0, tempo = 1;
+        for (int i = 0; i <= n; i++) 
+            adj[i].clear (), seen[i] = 0, seen_res[i] = 0;
+
         for (int i = 0; i < m; i++) {
             int u, v, c;
             scanf (" %d %d %d", &u, &v, &c);
-            add_edge (u, v, c, 0);
+            add_edge (u, v, c, c);
         }
 
         int ans = ford_fulkerson ();
 
         dfs_res (1);
+
+        for (int i = 1; i <= n; i++) {
+            if (!seen_res[i]) continue;
+            for (int j = 0; j < adj[i].size (); j++) {
+                edge ed = edges[adj[i][j]];
+                if (!seen_res[ed.to] && ed.c == ed.f) 
+                    printf ("%d %d\n", i, ed.to);
+            }
+        }
+        printf ("\n");
     }
 }
 
--- desafios/aula18/c.cpp
+++ desafios/aula18/c.cpp
@@ -17,42 +17,35 @@ const int MAXN = 10004;
 const int modn = 1000000007;
 
 int n, k, s;
+int v[25];
+int me[25][15][200];
 
-int dp (int i, int mask) {
-    int sum = 0;
-    for (int i = 0; i < 20; i++) {
-        if ((i & mask) != 0) 
-            sum += i+1;
-    }
-
-    if (i == k) {
+int dp (int i, int res, int sum) {
+    if (res == k) {
         if (sum == s) return 1;
-        else return 0;
+        return 0;
     }
-    if (me[qtd][sum] != -1) return me[qtd][sum];
+    if (i == n) return 0;
 
-    for (int i = 0; i < 20; i++) {
-        if ((i & mask) != 0) {
-            
-        }
+    if (me[i][res][sum] != -1) return me[i][res][sum];
+
+    int ans = dp (i+1, res, sum);
+    if (sum + v[i] <= s) {
+        ans += dp (i+1, res+1, sum+v[i]);
     }
 
-    return me[qtd][sum] = ans;
+    return me[i][res][sum] = ans;
 }
 
 int main() {
+    for (int i = 0; i < 20; i++) 
+        v[i] = i+1;
+
     while (scanf ("%d %d %d", &n, &k, &s)) {
         if (n == 0 && k == 0 && s == 0) break;
-
-        int val = 0;
-        for (int i = n; i > 0; i--) {
-            int a = 0;
-            if (val >= i) 
-                a = i;
-
-            val = val*10 + 
-        }
-        dp (k, s);
+        memset (me, -1, sizeof me);
+              
+        printf ("%d\n", dp (0, 0, 0)); 
     }
 }
 
